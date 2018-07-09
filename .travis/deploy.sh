93d9ac661f8b5d4e777e9923a72d850fdb4fa514
tobeorla <tobeorla@users.noreply.github.com>
2018-07-09 16:09:31 +0200

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,7 +14,7 @@ git config --global user.email "$GH_USER_EMAIL"
 git config --global user.name "$GH_USER_NAME"
 
 set +x
-git remote set-url origin https://ruvelro:$GH_TOKEN@github.com/ruvelro/TV-Online-TDT-Spain
+git remote set-url origin https://tobeorla:$GH_TOKEN@github.com/tobeorla/TV-Online-TDT-Spain
 set -x
 
 ## UPLOAD NEW HOSTS
