f3d457bc42bef5c9e9f33a3b42f6f000dde6a975
tobeorla <tobeorla@users.noreply.github.com>
2018-09-13 13:35:31 +0200

    Show why it fails

--- .travis/pingStreams.php
+++ .travis/pingStreams.php
@@ -2,7 +2,7 @@
 
 $eventType = getenv("TRAVIS_EVENT_TYPE");
 if($eventType != "cron" && $eventType != "pull_request") {
-    die("Not comming from a cron|pull_request");
+    die("Not coming from a cron|pull_request");
 }
 
 $streams = json_decode(file_get_contents('tv-spain.json'));
@@ -13,7 +13,7 @@ $readme .= "## Status Update: **" . date("Y-m-d") . "**\n\n";
 $readme .= "Status | Canal | URL\n--- | --- | ---\n";
 
 foreach ($streams as $stream) {
-    echo "Checking " . $stream->name . " - " . $stream->link_m3u8 . "\n";
+    debug("Checking " . $stream->name . " - " . $stream->link_m3u8);
     $status = ":red_circle:";
 
     if (isAlive($stream->link_m3u8)) {
@@ -25,6 +25,10 @@ foreach ($streams as $stream) {
 
 file_put_contents($readmeFile, $readme);
 
+function debug ($line) {
+    echo $line . "\n";
+}
+
 function isAlive ($url) {
     $ch = curl_init($url);
     curl_setopt($ch,  CURLOPT_RETURNTRANSFER, TRUE);
@@ -34,7 +38,12 @@ function isAlive ($url) {
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
 
-    if($httpCode != 200 ||substr($response, 0, 7) !== "#EXTM3U") {
+    if($httpCode != 200) {
+        debug("Error - HTTP code: " . $httpCode);
+        return false;
+    }
+    if(substr($response, 0, 7) !== "#EXTM3U") {
+        debug("Error - Response: " . $response);
         return false;
     }
 
